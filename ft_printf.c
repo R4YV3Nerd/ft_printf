793ce3b3fd841cda5eb217119bce18c9eb79c848
Mohamed Aitoumghar <maitoumg@e1r13p11.1337.ma>
2024-12-07 14:01:56 +0100

    edit04

--- ft_printf.c
+++ ft_printf.c
@@ -1,45 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maitoumg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 11:27:03 by maitoumg          #+#    #+#             */
+/*   Updated: 2024/12/07 13:58:09 by maitoumg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int handle_format_specifier(char specifier, va_list args)
+int	handle_string(va_list args)
 {
-    int count = 0;
-    if (specifier == 'd' || specifier == 'i')
-        count += ft_putnbr(va_arg(args, int));
-    else if (specifier == 's')
-        count += ft_putstr(va_arg(args, char *));
-    else if (specifier == 'x')
-        count += ft_puthex(va_arg(args, unsigned int));
-    else if (specifier == 'X')
-        count += ft_puthex_uppercase(va_arg(args, unsigned int));
-    else if (specifier == 'p')
-        count += ft_putpointer(va_arg(args, void *));
-    else if (specifier == 'u')
-        count += ft_putnumber_base(va_arg(args, unsigned int), 10, 0);
-    else if (specifier == '%')
-        count += ft_putchar('%');
-    else if (specifier == 'c')
-        count += ft_putchar((char)va_arg(args, int));
-    return count;
+	char	*str;
+
+	*str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(null)";
+	return (ft_putstr(str));
 }
 
-int ft_printf(const char *format, ...)
+int	handle_format_specifier(char specifier, va_list args)
 {
-    va_list args;
-    va_start(args, format);
-    
-    int count = 0;
-    while (*format)
-    {
-        if (*format == '%' && *(format + 1))
-        {
-            format++;
-            count += handle_format_specifier(*format, args);
-        }
-        else
-            count += ft_putchar(*format);
-        format++;
-    }
+	char	*str;
+	int		count;
 
-    va_end(args);
-    return count;
+	count = 0;
+	if (specifier == 'd' || specifier == 'i')
+		count += ft_putnbr(va_arg(args, int));
+	else if (specifier == 's')
+		count += handle_string(args);
+	else if (specifier == 'x')
+		count += ft_puthex(va_arg(args, unsigned int));
+	else if (specifier == 'X')
+		count += ft_puthex_uppercase(va_arg(args, unsigned int));
+	else if (specifier == 'p')
+		count += ft_putpointer(va_arg(args, void *));
+	else if (specifier == 'u')
+		count += ft_putnumber_base(va_arg(args, unsigned int), 10, 0);
+	else if (specifier == '%')
+		count += ft_putchar('%');
+	else if (specifier == 'c')
+		count += ft_putchar((char)va_arg(args, int));
+	return (count);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int	count;
+	va_list	args;
+
+	va_start(args, format);
+	count = 0;
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1))
+		{
+			format++;
+			count += handle_format_specifier(*format, args);
+		}
+		else
+			count += ft_putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
